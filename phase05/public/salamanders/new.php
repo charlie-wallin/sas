25f2d7f8842d47c8e07167afa8613c2fb8ea125b
Charlie Wallin <charliekwallin@gmail.com>
2024-04-15 16:28:07 -0400

    Phase05 was not working so I downloaded a working version from SiteGround.

--- phase05/public/salamanders/new.php
+++ phase05/public/salamanders/new.php
@@ -7,7 +7,7 @@ if(is_post_request()) {
     $salamander = [];
     $salamander['name'] = $_POST['name'] ?? '';
     $salamander['habitat'] = $_POST['habitat'] ?? '';
-    $salamander['description'] = $_POST['description'] ?? '';
+    $salamander['description'] = $_POST['description'];
 
     $result = insert_salamander($salamander);
     if($result === true) {
@@ -18,7 +18,6 @@ if(is_post_request()) {
     }
 }
 
-// First time in using GET and show the form
 
 $pageTitle = 'Create Salamander'; ?>
 
@@ -26,16 +25,16 @@ $pageTitle = 'Create Salamander'; ?>
     <?= display_errors($errors); ?>
     <form action="<?= url_for('salamanders/new.php'); ?>" method="post">
     <label for="name">
-        <p>Name:<br> <input type="text" name="name" value="<?= h($salamander['name']); ?>"></p>
+        <p>Name:<br> <input type="text" name="name" value=""></p>
     </label>
     <label for="habitat">
         <p>Habitat: <br>
-            <textarea rows="4" cols="50" name="habitat" value=""><?= h($salamander['habitat']); ?></textarea>
+            <textarea rows="4" cols="50" name="habitat" value=""></textarea>
         </p>
     </label>
     <label for="description">
         <p>Description:<br>
-            <textarea rows="4" cols="50" name="description" value=""><?= h($salamander['description']); ?></textarea> 
+            <textarea rows="4" cols="50" name="description" value=""></textarea> 
         </p>
     </label>
     <lable for="submit">
